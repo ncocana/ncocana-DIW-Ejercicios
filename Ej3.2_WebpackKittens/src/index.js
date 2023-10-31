9522c8e1dbd8a9105ee03225020915d835777cb7
ncocana <ncocana@cifpfbmoll.eu>
2023-10-31 17:48:40 +0100

    feat(Ej3.2_WebpackKittens): finish exercise Webpack Kittens

---
+++ Ej3.2_WebpackKittens/src/index.js
@@ -0,0 +1,5 @@
+import './css/application.css';
+import './sass/application.scss';
+// import './js/application.js';
+// import './js/jquery-2.1.0.min.js';
+// import './js/modernizr-2.5.2.min.js';
---
+++ Ej3.2_WebpackKittens/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Kittens "R" Us',
+        // Load a custom template (lodash by default)
+        template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+      runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ],
+    },
+};
